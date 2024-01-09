8e1579b7ee8a3118b30274d6345c269757714a81
Shakhram <shakag6@gmail.com>
2024-01-09 14:00:31 -0500

    Create log in component

--- cafe-depot/src/App.js
+++ cafe-depot/src/App.js
@@ -4,6 +4,7 @@ import Navbar from "./components/navbar/Navbar";
 import Landing from "./components/landing/Landing";
 import Utensils from "./components/shop/Utensils";
 import SignUp from "./components/user/SignUp";
+import LogIn from "./components/user/LogIn";
 import "./App.css";
 
 function App() {
@@ -15,6 +16,7 @@ function App() {
           <Route exact path="/" element={<Landing />} />
           <Route exact path="/shop/utensils" element={<Utensils />} />
           <Route exact path="/sign-up" element={<SignUp />} />
+          <Route exact path="/log-in" element={<LogIn />} />
           {/* <Route exact path="/shop/cups" element={<Cups />} /> */}
           {/* <Route exact path="/shop/plates" element={<Plates />} /> */}
           {/* <Route exact path="/shop//espresso-machines" element={<EspressoMachines />} /> */}
--- cafe-depot/src/components/user/LogIn.js
+++ cafe-depot/src/components/user/LogIn.js
@@ -2,12 +2,10 @@ import { useState } from "react";
 import { useNavigate } from "react-router-dom";
 import "../../styles/SignUp.css";
 
-export default function SignUp() {
+export default function LogIn() {
   const [formData, setFormData] = useState({
     username: "",
     password: "",
-    email: "",
-    address: "",
   });
   let navigate = useNavigate();
   const [loading, setLoading] = useState(false);
@@ -20,7 +18,7 @@ export default function SignUp() {
     e.preventDefault();
     try {
       const response = await fetch(
-        "https://cafe-depot-backend.onrender.com/api/users/sign-up",
+        "https://cafe-depot-backend.onrender.com/api/users/log-in",
         {
           method: "POST",
           mode: "cors",
@@ -33,15 +31,15 @@ export default function SignUp() {
       console.log("response ", response);
       if (response.ok) {
         const user = await response.json();
-        console.log("User created successfully:", user);
-        navigate("/log-in");
+        console.log("User logged in successfully:", user);
+        // redirect to profile page
       } else {
         const errorData = await response.json();
         throw errorData;
       }
     } catch (error) {
-      console.error("Error creating user:", error.message);
-      alert("Error creating user: " + error.message);
+      console.error("Error logging in user:", error.message);
+      alert("Error logging in user: " + error.message);
     }
   };
 
@@ -53,24 +51,18 @@ export default function SignUp() {
       <label>Password:</label>
       <input type="password" name="password" onChange={handleChange} required />
 
-      <label>Email:</label>
-      <input type="email" name="email" onChange={handleChange} required />
-
-      <label>Address:</label>
-      <input type="text" name="address" onChange={handleChange} required />
-
       {/* <button type="submit">Sign Up</button> */}
       <button type="submit" disabled={loading}>
-        {loading ? "Signing Up..." : "Sign Up"}
+        {loading ? "Logging In..." : "Log In"}
       </button>
 
       <label
         onClick={() => {
-          navigate("/log-in");
+          navigate("/sign-up");
         }}
         className="redirect-label"
       >
-        Returning user? <u>Log in here</u>
+        New user? <u>Sign up here</u>
       </label>
     </form>
   );
