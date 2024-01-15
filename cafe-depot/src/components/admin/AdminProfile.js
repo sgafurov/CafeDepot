96206b7f13712a0cc12450f5cd2a2fca3440df6d
Shakhram <shakag6@gmail.com>
2024-01-15 13:19:59 -0500

    Create basic outline for a product tile

---
+++ cafe-depot/src/components/admin/AdminProfile.js
@@ -0,0 +1,7 @@
+export default function AdminProfile(){
+    return(
+        <div>
+            <h1 className="text-center">Admin Profile</h1>
+        </div>
+    )
+}
--- cafe-depot/src/components/shop/Utensils.js
+++ cafe-depot/src/components/shop/Utensils.js
@@ -1,8 +1,45 @@
+import { useState } from "react";
+import utensilsImage from "../../assets/products/utensils.jpg";
+import "../../styles/Utensils.css";
+
 export default function Utensils() {
-    // get all utensils from DB and map out the utensils in boxes
-    return(
-        <div>
-           Utensils
-        </div>
-    )
+  const [quantity, setQuantity] = useState(1);
+
+  const increaseQuantity = () => {
+    setQuantity(quantity + 1);
+  };
+
+  const decreaseQuantity = () => {
+    if (quantity > 1) {
+      setQuantity(quantity - 1);
+    }
+  };
+
+  // get all utensils from DB and map out the utensils in boxes
+  return (
+      <div className="products-list">
+        <ul className="utensils-list">
+          {/*map through each utensil, create a li element with an onClick event that calls*/}
+          {/*addToCart(utensil) passing it the current utensil*/}
+          <li className="utensil-item">
+            <div>
+              <img src={utensilsImage} width={200} alt={"utensils"} />
+              <div className="details-text">
+                <p className="title">
+                  Metal utensils pack (50 forks, 50 spoons, 50 knives)
+                </p>
+                <p className="price">$10</p>
+              </div>
+              <div className="quantity-controls">
+                <button onClick={decreaseQuantity}>-</button>
+                <p>{quantity}</p>
+                <button onClick={increaseQuantity}>+</button>
+              </div>
+
+              <button className="add-to-cart">Add to Cart</button>
+            </div>
+          </li>
+        </ul>
+      </div>
+  );
 }
