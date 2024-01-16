7d18e5a4d02ddcff2bd05e79501214b28fa33a6e
Shakhram <shakag6@gmail.com>
2024-01-16 17:17:13 -0500

    Create config file for firebase

---
+++ cafe-depot/src/firebase.js
@@ -0,0 +1,20 @@
+import { initializeApp } from "firebase/app";
+import { getStorage } from "firebase/storage";
+import { getAnalytics } from "firebase/analytics";
+
+const firebaseConfig = {
+  apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
+  authDomain: "cafe-depot-4ec47.firebaseapp.com",
+  projectId: "cafe-depot-4ec47",
+  storageBucket: "cafe-depot-4ec47.appspot.com",
+  messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.REACT_APP_FIREBASE_APP_ID,
+  // For Firebase JavaScript SDK v7.20.0 and later, `measurementId` is an optional field
+  measurementId: "G-GYK7GXZV5E",
+};
+
+// Initialize Firebase
+
+const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
+const storage = getStorage(app, firebaseConfig.storageBucket);
