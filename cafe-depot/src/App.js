1122b74d0558baf6d20ef3cdcfdfbfc0b3f0a8e0
Shakhram <shakag6@gmail.com>
2024-01-19 23:58:14 -0500

    Add log out button and functionality. Track user log in status in App and Navbar to conditonally render things

--- cafe-depot/src/App.js
+++ cafe-depot/src/App.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Navbar from "./components/navbar/Navbar";
 import Landing from "./components/landing/Landing";
@@ -8,8 +8,30 @@ import Profile from "./components/user/Profile";
 import AdminProfile from "./components/admin/AdminProfile";
 import "./App.css";
 import ItemsByCategory from "./components/shop/ItemsByCategory";
+import { onAuthStateChanged } from "firebase/auth";
+import { auth } from "./firebase";
 
 function App() {
+  const [user, setUser] = useState(null);
+
+  useEffect(() => {
+    onAuthStateChanged(auth, (user) => {
+      if (user) {
+        // User is signed in, see docs for a list of available properties
+        // https://firebase.google.com/docs/reference/js/auth.user
+        const uid = user.uid;
+        setUser(user);
+        console.log("user is logged in");
+        // ...
+      } else {
+        // User is signed out
+        // ...
+        console.log("user is logged out");
+        setUser(null);
+      }
+    });
+  }, []);
+
   return (
     <div className="App">
       <Router>
@@ -19,8 +41,16 @@ function App() {
           <Route path="/shop/:category" element={<ItemsByCategory />} />
           <Route exact path="/sign-up" element={<SignUp />} />
           <Route exact path="/log-in" element={<LogIn />} />
-          <Route exact path="/profile" element={<Profile />} />
-          <Route exact path="/admin-profile" element={<AdminProfile />} />
+          <Route
+            exact
+            path="/profile"
+            element={user ? <Profile /> : <Landing />}
+          />
+          <Route
+            exact
+            path="/admin-profile"
+            element={user ? <AdminProfile /> : <Landing />}
+          />
           {/* <Route path="/product/:productId" component={ProductDetails} /> */}
         </Routes>
       </Router>
--- cafe-depot/src/components/navbar/Navbar.js
+++ cafe-depot/src/components/navbar/Navbar.js
@@ -1,15 +1,45 @@
+import { useState, useEffect } from "react";
 import logo from "../../assets/cafe-depot-high-resolution-logo-transparent.png";
 import bagIcon from "../../assets/icons/bag.png";
 import profileIcon from "../../assets/icons/user.png";
+import logOutIcon from "../../assets/icons/log-out.png";
 import { Link } from "react-router-dom";
 import { useNavigate } from "react-router-dom";
+import { onAuthStateChanged } from "firebase/auth";
+import { auth } from "../../firebase";
 import "../../styles/Navbar.css";
 
 export default function Navbar() {
   let navigate = useNavigate();
-  const goToLink = (linkName) => {
-    navigate(`/shop/${linkName}`);
+  const [user, setUser] = useState(null);
+
+  const handleLogout = async () => {
+    try {
+      await auth.signOut()
+      console.log("User logged out successfully");
+      navigate("/")
+    } catch (error) {
+      console.error("Error logging out:", error.message);
+    }
   };
+
+  useEffect(() => {
+    onAuthStateChanged(auth, (user) => {
+      if (user) {
+        // User is signed in, see docs for a list of available properties
+        // https://firebase.google.com/docs/reference/js/auth.user
+        const uid = user.uid;
+        setUser(user);
+        console.log("user is logged in");
+        // ...
+      } else {
+        // User is signed out
+        // ...
+        console.log("user is logged out");
+        setUser(null);
+      }
+    });
+  }, []);
   return (
     <div className="outer-navbar">
       <div className="inner-navbar">
@@ -78,6 +108,19 @@ export default function Navbar() {
               <li className="nav-link">
                 <img src={bagIcon} alt="Logo" className="logo" width={30} />
               </li>
+              {user && (
+                <li className="nav-link">
+                  <img
+                    src={logOutIcon}
+                    alt="Logo"
+                    className="logo"
+                    width={30}
+                    onClick={() => {
+                      handleLogout();
+                    }}
+                  />
+                </li>
+              )}
             </div>
           </li>
         </ul>
