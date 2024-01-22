49d72208dda997f927f855cee7f54560fb3ba21e
Shakhram <shakag6@gmail.com>
2024-01-22 13:28:01 -0500

    Navigate to the product component from landing page list items

--- cafe-depot/src/components/landing/Landing.js
+++ cafe-depot/src/components/landing/Landing.js
@@ -4,9 +4,11 @@ import utensilsImage from "../../assets/products/utensils.jpg";
 import cupsImage from "../../assets/products/cups-no-background.png";
 import platesImage from "../../assets/products/plates.jpg";
 import espressoMachineImage from "../../assets/products/espresso-machine.jpeg";
+import { useNavigate } from "react-router-dom";
 import "../../styles/Landing.css";
 
 export default function Landing() {
+  let navigate = useNavigate();
   const [categories, setCategories] = useState({
     names: ["Utensils", "Cups", "Plates", "Espresso Machines"],
     imagePaths: [utensilsImage, cupsImage, platesImage, espressoMachineImage],
@@ -19,9 +21,14 @@ export default function Landing() {
       <ul className="categories">
         {categories.names.map((name, index) => (
           <li key={index} className="category">
-            <div>
+            <div
+              style={{ cursor: "pointer" }}
+              onClick={() => {
+                navigate(`/shop/${name.toLowerCase()}`);
+              }}
+            >
               <p>{name}</p>
-              <img src={categories.imagePaths[index]} width={200} alt={name}/>
+              <img src={categories.imagePaths[index]} width={200} alt={name} />
             </div>
           </li>
         ))}
