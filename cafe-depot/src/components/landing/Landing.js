bfe7a0bc5dc6c5047ef773646cf916cd54ae0cdd
Shakhram <shakag6@gmail.com>
2024-10-02 21:07:09 -0400

    Create slideshow for landing

--- cafe-depot/src/components/landing/Landing.js
+++ cafe-depot/src/components/landing/Landing.js
@@ -1,5 +1,7 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import promoBanner from "../../assets/promo-banner.jpg";
+import espressoEquipmentImage from "../../assets/espresso-equipment.jpg"
+import cafeInteriorImage from "../../assets/cafe-interior.jpg"
 import utensilsImage from "../../assets/products/utensils.jpg";
 import cupsImage from "../../assets/products/cups-no-background.png";
 import platesImage from "../../assets/products/plates.jpg";
@@ -14,9 +16,30 @@ export default function Landing() {
     imagePaths: [utensilsImage, cupsImage, platesImage, espressoMachineImage],
   });
 
+  const images = [promoBanner, cafeInteriorImage, espressoEquipmentImage];
+  const [currentImageIndex, setCurrentImageIndex] = useState(0);
+
+
+  useEffect(() => {
+    const interval = setInterval(() => {
+      setCurrentImageIndex((prevIndex) => (prevIndex + 1) % images.length);
+    }, 3000); // Change image every 3 seconds
+
+    return () => clearInterval(interval);
+  }, [images.length]);
+
   return (
     <div className="landing">
-      <img className="promoBanner" src={promoBanner}></img>
+      {/* <img className="promoBanner" src={promoBanner}></img> */}
+
+      <div className="slideshow">
+        <img
+          className="slideshowImage"
+          src={images[currentImageIndex]}
+          alt="Promo"
+        />
+      </div>
+      
       <h1>Shop By Category</h1>
       <ul className="categories">
         {categories.names.map((name, index) => (
